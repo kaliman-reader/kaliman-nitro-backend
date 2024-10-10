9c0bbebd2087679bb13adf7a8c41e8d09a046c2e
Misael Abanto <josemisaelabanto@gmail.com>
2024-10-09 19:14:33 -0500

    fix(backend/routes): correct method name to fetch prefixes list
    - Changed method call from getFolders to getPrefixes in prefixes.ts
    - Removed unused function definition for getFolders from prefixes.ts

--- routes/prefixes.ts
+++ routes/prefixes.ts
@@ -4,18 +4,6 @@ export default eventHandler(async (event) => {
   const s3 = new S3({});
   const query = getQuery(event);
   const prefix = query.prefix as string;
-  const prefixes = await getFolders(s3, prefix);
+  const prefixes = await getPrefixes(s3, prefix);
   return prefixes;
 });
-
-async function getFolders(s3: S3, prefix?: string) {
-  const result = await s3.listObjects({
-    Bucket: process.env.BUCKET_NAME,
-    Prefix: prefix,
-    Delimiter: "/",
-  });
-  if (result.CommonPrefixes && result.CommonPrefixes?.length > 0) {
-    return result.CommonPrefixes;
-  }
-  return result.Contents;
-}
